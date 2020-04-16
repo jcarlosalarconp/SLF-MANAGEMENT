47dcf195fbe602e1aee0a2eb2f909084aa889348
Juan Carlos Alarcón Pedraza <48281276+jcarlosalarconp@users.noreply.github.com>
2020-04-16 14:04:56 +0200

    Avance en RecyclerView - Advances in RecyclerView
    Pruebas de varios intentos para conseguir la lista deseada en el PersonalFragment
    -
    Many attempts to finish PersonalFragment's list

--- admin/app/src/main/java/com/example/slf_management/adapter/PersonalAdapter.kt
+++ admin/app/src/main/java/com/example/slf_management/adapter/PersonalAdapter.kt
@@ -1,23 +1,68 @@
 package com.example.slf_management.adapter
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.example.slf_management.R
 import com.example.slf_management.items.PersonalItem
 import kotlinx.android.synthetic.main.item_personal.view.*
 
-class PersonalAdapter(private val mContext: Context, private val listaPersonal: List<PersonalItem>) : ArrayAdapter<PersonalItem>(mContext, 0, listaPersonal) {
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        val layout = LayoutInflater.from(mContext).inflate(R.layout.item_personal, parent, false)
 
-        val personal = listaPersonal[position]
+class PersonalAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>()
+{
 
-        layout.nombre.text = personal.nombre
-        layout.imagen.setImageResource(personal.imagen)
+    private var personal: List<PersonalItem> = ArrayList()
 
-        return layout
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return BlogViewHolder(
+                LayoutInflater.from(parent.context).inflate(R.layout.item_personal, parent, false)
+        )
     }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when(holder) {
+
+            is BlogViewHolder -> {
+                holder.bind(personal.get(position))
+            }
+
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return personal.size
+    }
+
+    fun submitList(blogList: List<PersonalItem>){
+        personal = blogList
+    }
+
+    class BlogViewHolder
+    constructor(
+            itemView: View
+    ): RecyclerView.ViewHolder(itemView){
+
+        val imagen = itemView.imagen
+        val nombre = itemView.nombre
+
+
+        fun bind(personalItem: PersonalItem){
+
+            val requestOptions = RequestOptions()
+                    .placeholder(R.drawable.ic_launcher_background)
+                    .error(R.drawable.ic_launcher_background)
+
+            Glide.with(itemView.context)
+                    .applyDefaultRequestOptions(requestOptions)
+                    .load(personalItem.imagen)
+                    .into(imagen)
+            nombre.setText(personalItem.nombre)
+
+        }
+
+    }
+
 }
--- admin/app/src/main/java/com/example/slf_management/fragments/PersonalFragment.kt
+++ admin/app/src/main/java/com/example/slf_management/fragments/PersonalFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 import com.example.slf_management.R
@@ -17,23 +18,11 @@ import com.example.slf_management.items.PersonalItem
  */
 class PersonalFragment : Fragment() {
     private lateinit var recyclerView: RecyclerView
+    private lateinit var personalAdapter: PersonalAdapter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-
-        val view = inflater.inflate(R.layout.fragment_personal, container, false)
-        recyclerView = view!!.findViewById(R.id.recyclerViewPersonal) as RecyclerView
-
-        val personal = PersonalItem("Juan Carlos", R.drawable.personal_icon)
-        val personal2 = PersonalItem("Javier", R.drawable.personal_icon)
-
-        val listaPersonal = listOf(personal, personal2)
-
-        val adapter = PersonalAdapter(inflater.context, listaPersonal)
-
-        recyclerView.adapter = adapter
-
-        return view
+    return view
     }
 
 }
